480afe12aa3317142168274b461086b54cd87298
Hank McGill <henrymcgill@gmail.com>
2023-04-25 15:32:28 -0400

    added comments and refactored

--- server/server.js
+++ server/server.js
@@ -6,33 +6,40 @@ const pgController = require('./controller/pgController');
 const path = require('path');
 // const bcrypt = require('bcrypt');
 
+// set up server
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, './public')));
 
+// GET handler
 app.get('/', pgController.getTasks, (req, res) => {
 	console.log(res.locals.table);
 	res.status(200).json(res.locals.table);
 });
 
+// POST handler:
 app.post('/', pgController.postTask, pgController.getTasks, (req, res) => {
 	console.log(res.locals.table);
 	res.status(200).json(res.locals.table);
 });
 
-app.delete('/', pgController.deleteTask, pgController.getTasks, (req, res) => {
-	console.log(res.locals.table);
-	res.status(200).json(res.locals.table);
-});
-
+// PATCH handler:
 app.patch('/', pgController.updateTask, pgController.getTasks, (req, res) => {
 	console.log(res.locals.table);
 	res.status(200).json(res.locals.table);
 });
 
+// DELETE handler:
+app.delete('/', pgController.deleteTask, pgController.getTasks, (req, res) => {
+	console.log(res.locals.table);
+	res.status(200).json(res.locals.table);
+});
+
+// 404 handler:
 app.get('/', (req, res) => res.status(404).send('Error: page not found'));
 
+// ERROR handler:
 app.use((err, req, res, next) => {
 	const defaultErr = {
 		log: 'Express error handler caught unknown middleware error',
@@ -44,6 +51,7 @@ app.use((err, req, res, next) => {
 	return res.status(errorObj.status).send(JSON.stringify(errorObj.message));
 });
 
+// server listen:
 app.listen(PORT, () => {
 	console.log(`listening on port: ${PORT}`);
 });
