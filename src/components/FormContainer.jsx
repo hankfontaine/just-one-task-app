153b07f6ced2e871c88b0968724b69c0d6a46eee
Hank McGill <henrymcgill@gmail.com>
2023-04-27 17:53:29 -0400

    finished project

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -5,16 +5,21 @@ import InputContainer from './InputContainer';
 
 export default function FormContainer (props) {
   return (
-    <main >
-      <input aria-label='input' placeholder='I want to...' minLength={1} maxLength ={35} value={props.currentTask} onChange={e => { props.setCurrentTask(e.target.value); props.setUserNotification('submit'); }}></input>
-    <div>
-    <button onClick={() => props.handleComplete()}>{props.userNotification}</button>
+    <main className="cursor-pointer main italic font-bold">
+      <div className='drop-shadow-xl'>
+      <input className='italic text-9xl font-extrabold
+            bg-gradient-to-r bg-clip-text  text-transparent
+            from-indigo-500 via-purple-500 to-indigo-500
+            animate-text' aria-label='input' placeholder='I want to...' minLength={1} maxLength ={35} value={props.currentTask} onChange={e => { props.setCurrentTask(e.target.value); props.setUserNotification('submit'); }}></input>
+      </div>
+    <div className='mt-20 ...'>
+    <button className='hover:text-lime-800' onClick={() => props.handleComplete()}>{props.userNotification}</button>
     </div>
     <div>
-    <button onClick={() => props.handleDelete()}>✗</button>
+    <button className='hover:text-orange-800 font-light' onClick={() => props.handleDelete()} >✗</button>
     </div>
     <div>
-    <button onClick={() => props.handleGet()}>accomplishments</button>
+    <button className='hover:text-orange-800 font-light' onClick={() => props.handleGet()}>accomplishments</button>
     </div>
     </main>
   );
--- src/components/TasksListContainer.jsx
+++ src/components/TasksListContainer.jsx
@@ -6,10 +6,12 @@ export default function TasksListContainer (props) {
   const propsArr = [];
 
   for (let i = 0; i < props.tasksArr.length; i++) {
-    propsArr.push(<li>{props.tasksArr[i]}</li>);
+    propsArr.push(<li className='italic' >{props.tasksArr[i]}</li>);
   }
 
   return (
+    <div className='#334155 tasks-container'>
     <ol className='taskList'>{propsArr}</ol>
+    </div>
   );
 }
